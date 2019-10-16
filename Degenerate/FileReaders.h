89a25bab050f7bd5be2547410b2509d2534d19a4
Spencer <Spencer@LAPTOP-QF032TM9>
2019-10-16 14:01:12 -0400

    Name Change

--- Degenerate/FileReaders.h
+++ Degenerate/FileReaders.h
@@ -4,4 +4,4 @@
 #include <vector>
 void ReadGameObjectsFromFile(std::string File, std::vector<GameObject*>& vecGameObjects, bool clearExistingObjects = 1);
 void WriteGameObjectsToFile(std::string File, std::vector<GameObject*> vecGameObjects);
-void ReadLightsFromFile(std::string File, std::vector<Light*>& lights, bool clearExistingLights = 1);
+void ReadLightsFromFile(std::string File, std::vector<Light*>& vecLights, bool clearExistingLights = 1);
